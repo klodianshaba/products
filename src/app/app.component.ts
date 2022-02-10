99e90289296a36836b2d2c48d91bc6f952195652
klodian shaba <dev.klodian@gmail.com>
2022-02-10 08:47:38 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {State} from "./state/reducers";
+import {selectAllProducts} from "./state/selectors/products.selectors";
+import {ProductService} from "./shared/services/product.service";
+import {loadProducts} from "./state/actions/products.actions";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud';
+  constructor(private store: Store<State>, private productService: ProductService) {
+    this.store.select(selectAllProducts).subscribe(products => {
+      if(products){
+        console.log(products);
+      }
+    });
+
+    this.productService.getProducts().subscribe(products => {
+      console.log(products);
+      this.store.dispatch(loadProducts({products:products}));
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './state/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
